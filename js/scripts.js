0e282853f02554444d2f5656067178e586502649
Zilfa Cyamani <zilcyam@gmail.com>
2020-10-12 13:55:06 +0200

    indentation

--- js/scripts.js
+++ js/scripts.js
@@ -1,91 +1,91 @@
-function PizzaList(size, crust, topping=[]) {
+function PizzaList(size, crust, topping = []) {
     this.size = size;
     this.crust = crust;
     this.topping = topping;
 }
 
-PizzaList.prototype.wholePizza = function(){
+PizzaList.prototype.wholePizza = function () {
 
-var priceTheSize;
-var priceTheCrust;
-if(this.size == "theSmall") {
-    priceTheSize = 100;
-}
-else if(this.size == "theMedium") {
-    priceTheSize = 200;
-}
-else{
-    priceTheSize = 300;
-}
-
-if(this.crust == "theCrispy") {
-    priceTheCrust = 50;
-}
-else if(this.crust == "theStuffed"){
-    priceTheCrust = 100;
-}
-else {
-    priceTheCrust = 200;
-}
-
-var toppingPr = [];
-
-for (var cash = 0; cash < this.topping.length; cash++) {
-    if(this.topping[cash] == "onion"){
-        var priceTheOnions = 50;
-        toppingPr.push(priceTheOnions);
+    var priceTheSize;
+    var priceTheCrust;
+    if (this.size == "theSmall") {
+        priceTheSize = 100;
     }
-}
-
-for (var cash = 0; cash < this.topping.length; cash++) {
-    if(this.topping[cash] == "brocollini"){
-        var priceTheBrocollini  = 100;
-        toppingPr.push(priceTheBrocollini);
+    else if (this.size == "theMedium") {
+        priceTheSize = 200;
     }
-}
-console.log(priceTheBrocollini + "bren")
-
-for (var cash = 0; cash < this.topping.length; cash++) {
-    if(this.topping[cash] == "garlic"){
-        var priceTheGarlic = 80;
-        toppingPr.push(priceTheGarlic);
+    else {
+        priceTheSize = 300;
     }
-}
-console.log(toppingPr + "ren");
 
-var toppingPrice = 0;
-var i = 0;
-while(i < toppingPr.length){
-    toppingPrice = toppingPrice + toppingPr[i]
-    i++;
-}
-console.log(toppingPrice + "thar");
+    if (this.crust == "theCrispy") {
+        priceTheCrust = 50;
+    }
+    else if (this.crust == "theStuffed") {
+        priceTheCrust = 100;
+    }
+    else {
+        priceTheCrust = 200;
+    }
 
-var theTotal = priceTheSize + priceTheCrust + toppingPrice;
-return theTotal;
+    var toppingPr = [];
+
+    for (var cash = 0; cash < this.topping.length; cash++) {
+        if (this.topping[cash] == "onion") {
+            var priceTheOnions = 50;
+            toppingPr.push(priceTheOnions);
+        }
+    }
+
+    for (var cash = 0; cash < this.topping.length; cash++) {
+        if (this.topping[cash] == "brocollini") {
+            var priceTheBrocollini = 100;
+            toppingPr.push(priceTheBrocollini);
+        }
+    }
+    console.log(priceTheBrocollini + "bren")
+
+    for (var cash = 0; cash < this.topping.length; cash++) {
+        if (this.topping[cash] == "garlic") {
+            var priceTheGarlic = 80;
+            toppingPr.push(priceTheGarlic);
+        }
+    }
+    console.log(toppingPr + "ren");
+
+    var toppingPrice = 0;
+    var i = 0;
+    while (i < toppingPr.length) {
+        toppingPrice = toppingPrice + toppingPr[i]
+        i++;
+    }
+    console.log(toppingPrice + "thar");
+
+    var theTotal = priceTheSize + priceTheCrust + toppingPrice;
+    return theTotal;
 }
 
-$("form").submit(function(e){
- e.preventDefault();
- var sizeSelect = $("select#theSize").val();
- var crustSelect = $("select#theCrust").val();
- var toppingSelect = [];
- var quantitySelect = $("input#quantity").val();
- toppingSelect = $("input:checkbox:checked").map(function(){
-    return $(this).val();
- }).get();
- var newCustomer = new PizzaList(sizeSelect,crustSelect,toppingSelect);
- $('#display').text(newCustomer.wholePizza());
- console.log(newCustomer.wholePizza());
- var dan = newCustomer.wholePizza()*quantitySelect;
- $('#result').text(dan);
- console.log(quantitySelect + "dian");
+$("form").submit(function (e) {
+    e.preventDefault();
+    var sizeSelect = $("select#theSize").val();
+    var crustSelect = $("select#theCrust").val();
+    var toppingSelect = [];
+    var quantitySelect = $("input#quantity").val();
+    toppingSelect = $("input:checkbox:checked").map(function () {
+        return $(this).val();
+    }).get();
+    var newCustomer = new PizzaList(sizeSelect, crustSelect, toppingSelect);
+    $('#display').text(newCustomer.wholePizza());
+    console.log(newCustomer.wholePizza());
+    var dan = newCustomer.wholePizza() * quantitySelect;
+    $('#result').text(dan);
+    console.log(quantitySelect + "dian");
 
 })
- 
-function theFunc(){
+
+function theFunc() {
     var homeAddre = prompt("Please Enter Your Adrress!");
-    if(homeAddre){
-       alert("Your order will be delivered at " + homeAddre); 
+    if (homeAddre) {
+        alert("Your order will be delivered at " + homeAddre);
     }
 }
